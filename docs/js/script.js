2ab5d72a50503a8b130f0d14160248faa6774362
mrjokeller <jonikeller98@gmail.com>
2026-03-15 13:25:23 +0100

    add table design

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,6 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
             const tableBody = document.getElementById('ranking-body');
             data.forEach((player, index) => {
                 const row = document.createElement('tr');
+                // inserts a class according to the rank of the player (rank-1, rank-2, rank-3 for the podium)
+                if (index < 3) {
+                    row.classList.add(`rank-${index + 1}`);
+                } else if (index % 2 == 0) {
+                    row.classList.add('rank-even');
+                } else {
+                    row.classList.add('rank-uneven');
+                }
+                // html insertion
                 row.innerHTML = `
             <td>${index + 1}</td>
             <td>${player.player_name}</td>
